29ae1908845378e42d059cfaf753c330849ac110
NoteVote <bass4aj@gmail.com>
2015-09-22 14:46:52 -0500

    Initial upload
    Working Spotify authenticator

---
+++ NVBeta/Spot_test/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Spot_test
+//
+//  Created by uics15 on 9/22/15.
+//  Copyright © 2015 uiowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SPTAuthViewDelegate, SPTAudioStreamingPlaybackDelegate {
+
+    let kClientID = "ddf55f7bf8ec47e1a9a998c53207adb2"
+    let kCallbackURL = "notevote-login://callback"
+    let kTokenSwapURL = "http://localhost:1234/swap"
+    let kTokenRefreshURL = "http://localhost:1234/refresh"
+    
+    var player: SPTAudioStreamingController?
+    let spotifyAuthenticator = SPTAuth.defaultInstance()
+    
+    @IBAction func loginWithSpotify(sender: AnyObject) {
+        spotifyAuthenticator.clientID = kClientID
+        spotifyAuthenticator.requestedScopes = [SPTAuthStreamingScope]
+        spotifyAuthenticator.redirectURL = NSURL(string: kCallbackURL)
+        spotifyAuthenticator.tokenSwapURL = NSURL(string: kTokenSwapURL)
+        spotifyAuthenticator.tokenRefreshURL = NSURL(string: kTokenRefreshURL)
+        
+        let spotifyAuthenticationViewController = SPTAuthViewController.authenticationViewController()
+        spotifyAuthenticationViewController.delegate = self
+        spotifyAuthenticationViewController.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+        spotifyAuthenticationViewController.definesPresentationContext = true
+        presentViewController(spotifyAuthenticationViewController, animated: false, completion: nil)
+    }
+    
+    func loginWithSpotify() {
+        spotifyAuthenticator.clientID = kClientID
+        spotifyAuthenticator.requestedScopes = [SPTAuthStreamingScope]
+        spotifyAuthenticator.redirectURL = NSURL(string: kCallbackURL)
+        spotifyAuthenticator.tokenSwapURL = NSURL(string: kTokenSwapURL)
+        spotifyAuthenticator.tokenRefreshURL = NSURL(string: kTokenRefreshURL)
+        
+        let spotifyAuthenticationViewController = SPTAuthViewController.authenticationViewController()
+        spotifyAuthenticationViewController.delegate = self
+        spotifyAuthenticationViewController.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+        spotifyAuthenticationViewController.definesPresentationContext = true
+        presentViewController(spotifyAuthenticationViewController, animated: false, completion: nil)
+
+    }
+    // SPTAuthViewDelegate protocol methods
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didLoginWithSession session: SPTSession!) {
+        setupSpotifyPlayer()
+        loginWithSpotifySession(session)
+    }
+    
+    func authenticationViewControllerDidCancelLogin(authenticationViewController: SPTAuthViewController!) {
+        print("login cancelled")
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didFailToLogin error: NSError!) {
+        print("login failed")
+    }
+    
+    // SPTAudioStreamingPlaybackDelegate protocol methods
+    
+    private
+    
+    func setupSpotifyPlayer() {
+        player = SPTAudioStreamingController(clientId: spotifyAuthenticator.clientID) // can also use kClientID; they're the same value
+        player!.playbackDelegate = self
+        player!.diskCache = SPTDiskCache(capacity: 1024 * 1024 * 64)
+    }
+    
+    func loginWithSpotifySession(session: SPTSession) {
+        player!.loginWithSession(session, callback: { (error: NSError!) in
+            if error != nil {
+                print("Couldn't login with session: \(error)")
+                return
+            }
+            self.useLoggedInPermissions()
+        })
+    }
+    
+    func useLoggedInPermissions() {
+        let spotifyURI = "spotify:track:1WJk986df8mpqpktoktlce"
+        player!.playURIs([NSURL(string: spotifyURI)!], withOptions: nil, callback: nil)
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        loginWithSpotify(self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
