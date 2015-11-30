260def0c5c0bf1df08eb27f79e8a6937d8fd9ce4
NoteVote <bass4aj@gmail.com>
2015-11-29 21:30:04 -0600

    play/pause, continuous play
    songs play fifo in queue, play and pause works

--- NVBeta/Spot_test/HostRoomVC.swift
+++ NVBeta/Spot_test/HostRoomVC.swift
@@ -15,12 +15,37 @@ class HostRoomVC: UIViewController, SPTAudioStreamingPlaybackDelegate {
     @IBOutlet weak var navBarTitle: UILabel!
     @IBOutlet weak var trackTitle: UILabel!
     @IBOutlet weak var trackArtist: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
     //TODO: Make songQueue a list of Dictionaries. Each dictionary has title, artist, and votes as keys.
     var musicList:[[AnyObject]] = []
     var roomID = ""
     
     private var player:SPTAudioStreamingController?
     private let authController = SpotifyAuth()
+    
+    @IBAction func playPausePressed(sender: AnyObject) {
+        if (self.player!.isPlaying) {
+            
+            self.player!.setIsPlaying(false, callback: { (error:NSError!) -> Void in
+                if error != nil {
+                    print("Enabling playback got error \(error)")
+                    return
+                }
+            })
+            playPauseButton.setTitle("Play", forState: UIControlState.Normal)
+            
+        } else {
+            
+            self.player!.setIsPlaying(true, callback: { (error:NSError!) -> Void in
+                if error != nil {
+                    print("Enabling playback got error \(error)")
+                    return
+                }
+            })
+            playPauseButton.setTitle("Pause", forState: UIControlState.Normal)
+        }
+    }
+    
     
     func playUsingSession(sessionObj:SPTSession!){
         
@@ -39,9 +64,10 @@ class HostRoomVC: UIViewController, SPTAudioStreamingPlaybackDelegate {
             
             
             //TODO dynamic track URI
-            
-            
-            self.player?.playURI(NSURL(string: "spotify:track:5pY3ovFxbvAg7reGZjJQSp"), callback: { (error:NSError!) -> Void in
+            let currentTrack = self.musicList[0][0] as! String
+            self.musicList.removeAtIndex(0)
+            print(currentTrack)
+            self.player?.playURI(NSURL(string: currentTrack), callback: { (error:NSError!) -> Void in
                 if error != nil {
                     print("Track lookup got error \(error)")
                     return
@@ -56,48 +82,72 @@ class HostRoomVC: UIViewController, SPTAudioStreamingPlaybackDelegate {
     
     //fires whenever the track changes
     func audioStreaming(audioStreaming: SPTAudioStreamingController!, didChangeToTrack trackMetadata: [NSObject : AnyObject]!) {
-        
-        let albumURI = trackMetadata["SPTAudioStreamingMetadataAlbumURI"] as! String
-        trackTitle.text! = trackMetadata["SPTAudioStreamingMetadataTrackName"] as! String
-        trackArtist.text! = trackMetadata["SPTAudioStreamingMetadataArtistName"] as! String
-        
-        SPTAlbum.albumWithURI(NSURL(string: albumURI), session: nil) { (error:NSError!, albumObj:AnyObject!) -> Void in
-            let album = albumObj as! SPTAlbum
+        if trackMetadata == nil {
             
-            
-            //TODO: I dont understand this dispatch async thing
-            
-            if let imgURL = album.largestCover.imageURL as NSURL! {
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
-                    var error:NSError? = nil
-                    var coverImage = UIImage()
-                    
-                   if let imageData = NSData(contentsOfURL: imgURL){
-                        
-                        if error == nil {
-                            coverImage = UIImage(data: imageData)!
-                        }
+            //TODO: SELECT SONGS ON VOTES, SOMEHOW IMPLEMENT PLAYLIST INTEGRATION
+            if (!self.musicList.isEmpty) {
+                let currentTrack = self.musicList[0][0] as! String
+                self.musicList.removeAtIndex(0)
+                print(currentTrack)
+                self.player?.playURI(NSURL(string: currentTrack), callback: { (error:NSError!) -> Void in
+                    if error != nil {
+                        print("Track lookup got error \(error)")
+                        return
                     }
-                    
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        self.albumImage.image = coverImage
-                    })
                 })
             }
+        } else {
+            let albumURI = trackMetadata["SPTAudioStreamingMetadataAlbumURI"] as! String
+            trackTitle.text! = trackMetadata["SPTAudioStreamingMetadataTrackName"] as! String
+            trackArtist.text! = trackMetadata["SPTAudioStreamingMetadataArtistName"] as! String
             
+            SPTAlbum.albumWithURI(NSURL(string: albumURI), session: nil) { (error:NSError!, albumObj:AnyObject!) -> Void in
+                let album = albumObj as! SPTAlbum
+                
+                
+                //TODO: I dont understand this dispatch async thing
+                
+                if let imgURL = album.largestCover.imageURL as NSURL! {
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+                        var error:NSError? = nil
+                        var coverImage = UIImage()
+                        
+                       if let imageData = NSData(contentsOfURL: imgURL){
+                            
+                            if error == nil {
+                                coverImage = UIImage(data: imageData)!
+                            }
+                        }
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.albumImage.image = coverImage
+                        })
+                    })
+                }
+            }
         }
 
     }
     
     //fires when track stops playing
-    func audioStreaming(audioStreaming: SPTAudioStreamingController!, didStopPlayingTrack trackUri: NSURL!) {
-        let roomID = userDefaults.objectForKey("roomID") as! String
+   /* func audioStreaming(audioStreaming: SPTAudioStreamingController!, didStopPlayingTrack trackUri: NSURL!) {
+        /*let roomID = userDefaults.objectForKey("roomID") as! String
         serverLink.queueForRoomID(roomID){
             (result: [[AnyObject]]) in print(result)
             
-        }
+        }*/
+        let currentTrack = self.musicList[0][0] as! String
+        self.musicList.removeAtIndex(0)
+        print(currentTrack)
+        self.player?.playURI(NSURL(string: currentTrack), callback: { (error:NSError!) -> Void in
+            if error != nil {
+                print("Track lookup got error \(error)")
+                return
+            }
+            
+        })
 
-    }
+    }*/
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
@@ -111,6 +161,8 @@ class HostRoomVC: UIViewController, SPTAudioStreamingPlaybackDelegate {
         }
         
     }
+    
+    
 
     
     override func viewDidLoad() {
